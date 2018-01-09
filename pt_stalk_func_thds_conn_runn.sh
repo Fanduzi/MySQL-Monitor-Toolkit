6e4a17a79c92b7eff8c4a32a3ea7217afc27d342
Fan() <18501341937@163.com>
2018-01-09 14:19:59 +0800

    增加执行命令

--- pt_stalk_func_thds_conn_runn.sh
+++ pt_stalk_func_thds_conn_runn.sh
@@ -11,3 +11,5 @@ else if ($2 ~ /Uptime / && count >= 0){\
 { if ($1 > 450 || $2 > 400 ) counter++ ; } END {print counter}'
 }
 trg_plugin
+
+#/usr/bin/pt-stalk --function /data/scripts/bin/pt_stalk_func_thds_conn_runn.sh --threshold 0 --daemonize --cycles 3 --user=root --ask-pass --socket=/data/mysql/mysql.sock
